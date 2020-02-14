ff65bc751f9daa10fd6f7407a5eb883844e3b2b0
Jay <jial8@uw.edu>
2020-02-13 22:46:25 -0800

    hw5 progress 1

--- HW5/jial8_VI.py
+++ HW5/jial8_VI.py
@@ -1,16 +1,20 @@
-'''YourUWNetID_VI.py
-(rename this file using your own UWNetID.)
-
+'''jial8_VI.py
+Name: Jia-Jia (Jay) Lin
+Student ID: 1820474
+CSE415 Winter 2020
 Value Iteration for Markov Decision Processes.
 '''
 
 # Edit the returned name to ensure you get credit for the assignment.
 def student_name():
-   return "Your Lastname, Firstname" # For an autograder.
+   return "Lin, Jia-Jia" # For an autograder.
 
 Vkplus1 = {}
 Q_Values_Dict = {}
+one_step_ofVI_called = 0
 
+# 1 iteration of Value Iteration from the given MDP information plus the current state values,
+# which are provided in a dictionary Vk whose keys are states and whose values are floats.
 def one_step_of_VI(S, A, T, R, gamma, Vk):
    '''S is list of all the states defined for this MDP.
    A is a list of all the possible actions.
@@ -30,11 +34,27 @@ def one_step_of_VI(S, A, T, R, gamma, Vk):
    amount that the absolute value of any state's value is changed
    during this iteration.
    '''
-
    global Q_Values_Dict
-   
-   # return (Vkplus1, delta_max)
-   return (Vk, 0) # placeholder
+   # initial states
+   delta = 0
+   q = 0
+   v = -100000000000
+   # for every s a s' pairing, do the iteration thing
+   for s in S:
+      for a in A:
+         q = 0
+         for endState in S:
+            # find sums Q value first before V, use Q formula from homework
+            q += T(s, a, endState) * (R(s,a,endState)+gamma*Vk[endState])
+            Q_Values_Dict[(s,a)] = q;
+            # New V, given the max from Q sums
+            Vkplus1[state] = max(v, q);
+            # update delta to the largest change
+            change = Vkplus1[state] - Vk[state];
+            delta = max(delta, abs(change));
+   one_step_of_VI_called += 1
+   return Vkplus1, delta_max
+
 
 def return_Q_values(S, A):
    '''Return the dictionary whose keys are (state, action) tuples,
@@ -44,7 +64,12 @@ def return_Q_values(S, A):
    However, if no such call has been made yet, use S and A to
    create the answer dictionary, and use 0.0 for all the values.
    '''
-   return Q_Values_Dict # placeholder
+   if one_step_ofVI_called < 1:
+      # if there is nothing in dictionary yet, make everything 0.0
+      for s in S:
+         for a in A:
+            Q_Values_Dict[(s, a)] = 0.0
+   return Q_Values_Dict
 
 Policy = {}
 def extract_policy(S, A):
@@ -57,11 +82,24 @@ def extract_policy(S, A):
    global Policy
    Policy = {}
    # Add code here
+   if one_step_of_VI_called < 1:
+      return_Q_values(S,A)
+   # go through each state and action to see which action is the best
+   best_action = None
+   best_q = -100000000000
+   for s in S:
+      for a in A:
+         # is this the max q value so far
+         if best_q == min(best_q, Q_Values_Dict[(s,a)]):
+            best_q = Q_Values_Dict[(s,a)]
+            best_action = a
+         # update state with best action so far
+         Policy[s] = best_action
    return Policy
 
+'''Return the action that your current best policy implies for state s.'''
 def apply_policy(s):
-   '''Return the action that your current best policy implies for state s.'''
    global Policy
-   return None # placeholder
+   return Policy[s]
 
 
